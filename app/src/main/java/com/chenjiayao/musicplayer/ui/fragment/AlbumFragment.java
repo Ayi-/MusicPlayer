5dc1997d4a90c59c05b81cb9568836acccb835a5
GithubOnePiece <mail_one_piece@163.com>
2015-12-16 15:48:12 +0800

    motify something

---
+++ app/src/main/java/com/chenjiayao/musicplayer/ui/fragment/AlbumFragment.java
@@ -0,0 +1,9 @@
+package com.chenjiayao.musicplayer.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by chen on 2015/12/16.
+ */
+public class AlbumFragment extends Fragment{
+}
---
+++ app/src/main/java/com/chenjiayao/musicplayer/ui/fragment/SongFragment.java
@@ -0,0 +1,10 @@
+package com.chenjiayao.musicplayer.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by chen on 2015/12/16.
+ */
+public class SongFragment extends Fragment{
+
+}
